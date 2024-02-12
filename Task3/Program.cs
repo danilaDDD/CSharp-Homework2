3b560a35a2b4add771b74a5f137b8e31d45a54e6
danila nikitin <danila.n2015@yandex.ru>
2024-02-12 16:31:10 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = number % 10;
+number = number / 10;
+int secondDigit = number % 10;
+
+if(firstDigit > secondDigit)
+    System.Console.WriteLine(firstDigit);
+else
+    System.Console.WriteLine(secondDigit);
+
