5a7da1ebddd508453c0750eb07f813be4f0acc7d
danila nikitin <danila.n2015@yandex.ru>
2024-02-12 17:02:56 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+Console.Write("Введите число:");
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+int digit = number % 10;
+number /= 10;
+System.Console.Write(digit);
+
+while(number > 0)
+{
+    digit = number % 10;
+    number /= 10;
+    System.Console.Write("," + digit);
+}
